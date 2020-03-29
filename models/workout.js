378eca25aa55b97c2d0dca817d36b162e876aeef
Alex Elmore <alexelmore1014@gmail.com>
2020-03-28 23:39:51 -0400

    added html routes

--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,60 @@
 const mongoose = require ("mongoose");
 
 const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema ({
+day: {
+        
+type: Date,
+default: Date.now
+    },
+
+
+exercises: [
+    //This will allow the user to make a custom workout 
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter an exercise type"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter an exercise name"
+            }, 
+            duration:  {
+                type: Number,
+                trim: true,
+                required: "Enter total time of workout"
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
+
+},
+{
+    toJSON: {
+virtuals: true
+    }
+})
+//This will add together the total duration of all the exercises
+workoutSchema.virtual("totalDuration").get(function (){
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0)
+})
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,3 +1,14 @@
 const router = require("express").Router()
 const db = require("../models")
 const path = require("path")
+
+router.use("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+router.use("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+router.use("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+module.exports = router
