44084f9895ac5dbc0b1e8bdad08f2a22252e7b2c
Alex Elmore <alexelmore1014@gmail.com>
2020-03-28 23:54:31 -0400

    added workout.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,2 +1,61 @@
 const router = require("express").Router()
 const Workout = require("../models/workout.js")
+
+router.post("/api/workouts", ( req, res) => {
+    Workout.create({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+router.put("/api/workouts/:id", ({ body, params}, res)=> {
+Workout.findByIdAndUpdate(
+    params.id,
+    {$push: {exercises: body}
+
+          },
+          {
+              new: true, runValidators: true
+          }
+      )
+      .then(dbWorkout => {
+          res.json(dbWorkout);
+      })
+      .catch(err=> {
+          res.json(err)
+      })
+  })
+
+  router.get ("/api/workouts", (req,res) => {
+      Workout.find()
+      .then(dbWorkouts => {
+          res.json(dbWorkouts);
+      })
+      .catch(err => {
+          res.json(err)
+      })
+  })
+
+  router.get("/api/workouts/range", (req, res) => {
+      Workout.find({}).limit(9)
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err)
+        })
+  })
+//The user can delete a work out
+  router.delete("/api/workouts", ({ body },res)=> {
+      Workout.findByIdAndDelete(body.id)
+      .then(()=> {
+        res.json(true);
+      })
+      .catch (err => {
+          res.json(err)
+      })
+  })
+
+  module.exports = router
